70ef740a30e0275436c76f946e193bfa63c3da22
amitavsahu <amitavsahu77@gmail.com>
2023-03-03 20:22:15 +0530

    modified some codes

--- hello.java
+++ hello.java
@@ -9,9 +9,20 @@ class hello {
 };
 >>>>>>> d7cc2d4588cef4403aedc5a1c24e23a8ad4322c5
 
+<<<<<<< Updated upstream
 //task 120 changes
 class hi{
 int a=10;
 int b=20;
 int c=30;
 }
+=======
+//task - 101 changes
+m1(){
+
+}
+//task - 120 changes
+ m2 (){
+    
+ }
+>>>>>>> Stashed changes
